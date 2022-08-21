44228a761aa68d37cd27a84791a15a7f6904510f
Yarik <yarik747@yandex.ru>
2022-08-22 00:25:21 +0300

    Добавил поддержку токенов(перехватчик запросов)

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://10.react.pages.academy/six-cities';
 const REQUEST_TIMEOUTE = 5000;
@@ -7,6 +8,17 @@ export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUTE,
+  });
+
+  // Перехватчик для запросов
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
   });
 
   return api;
