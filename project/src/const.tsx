50d9dbeab894dda792de9441b4617e13e19bf5e9
Yarik <yarik747@yandex.ru>
2022-08-23 15:35:03 +0300

    Создал и подключил компонент для отображения ошибки

--- project/src/const.tsx
+++ project/src/const.tsx
@@ -27,7 +27,7 @@ export const CITIES = [
   'Dusseldorf',
 ];
 
-export const TIMEOUT_SHOW_ERROR = 10000;
+export const TIMEOUT_SHOW_ERROR = 20000;
 
 export const URL_MARKER_DEFAULT =
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
