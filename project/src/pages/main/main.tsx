5dd210811529a5d9f5c25d889b75d7f805aa76ba
Yarik <yarik747@yandex.ru>
2022-08-31 23:37:25 +0300

    Исправил main

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import DropDown from '../../components/drop-down-form/drop-down-form';
-import Logo from '../../components/logo/logo';
+import { useState, useMemo } from 'react';
+import DropDownForm from '../../components/drop-down-form/drop-down-form';
 import OfferList from '../../components/offerList';
-import { AppRoute } from '../../const';
 import { Offer, Point } from '../../types';
 import Map from './../../components/map/map';
 import CityList from './../../components/city-list/city-list';
 import { CITIES } from './../../const';
-import Email from './../../components/email/email';
 import { useSelector } from 'react-redux';
-import { offers } from '../../mocks/offers';
-import { setOfferList } from '../../store/action';
-import { useDispatch } from 'react-redux';
-
-// Функция возвращает массив предложений
-const getOffers = () => offers;
+// import { useDispatch } from 'react-redux';
+// import { logoutAction } from '../../store/api-actions';
+import Header from './../../components/header/header';
 
 function Main(): JSX.Element {
   const offerList: Offer[] = useSelector((state: any) => state.offerList);
+
+  const [city, setCity] = useState(CITIES[0]);
+
+  const offers = useMemo(
+    () => offerList.filter((offer) => offer.city.name === city),
+    [offerList, city]
+  );
 
   const [selectedPoint, setSelectedPoint] = useState<Point | undefined>(
     undefined
@@ -35,55 +35,31 @@ function Main(): JSX.Element {
     setSelectedPoint(currentPoint);
   };
   // Получаю название городов из стейта(хранилища)
-  const currentCity = useSelector((state: any) => state.city);
+
   const countOffers = offerList.length;
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    const list = getOffers();
-    dispatch(setOfferList(list));
-  }, [currentCity, dispatch]);
-  // console.log(offers);
+  // dispatch(logoutAction());
+
   return (
     <>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Email />
-                </li>
-
-                <li className="header__nav-item">
-                  <Link to={AppRoute.Login} className="header__nav-link">
-                    <span className="header__signout">Sign out</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
-        <CityList cities={CITIES} />
+        <CityList cities={CITIES} city={city} onCityChange={setCity} />
+
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">
-                {countOffers} places to stay in {currentCity}
+                {countOffers} places to stay in {city}
               </b>
 
-              <DropDown />
+              <DropDownForm />
 
-              <OfferList offers={offerList} onListItemHover={onListItemHover} />
+              <OfferList offers={offers} onListItemHover={onListItemHover} />
             </section>
             <div className="cities__right-section">
               <Map points={points} selectedPoint={selectedPoint} />
